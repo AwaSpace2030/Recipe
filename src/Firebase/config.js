7a97b93b66bf54791d697a5c36b38d38922dd449
Awadh Aldeabi <awdec238@gmail.com>
2026-01-03 14:12:55 +0100

    Update project after fixing firebase key

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -2,18 +2,18 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDgynpTrrcEMYbIyR6b6gwT70ORR4Bq5e4",
-  authDomain: "recipe-c93c8.firebaseapp.com",
-  projectId: "recipe-c93c8",
-  storageBucket: "recipe-c93c8.appspot.com",
-  messagingSenderId: "10127320862",
-  appId: "1:10127320862:web:530d56f97479642b178b0f",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
+console.log("Firebase API Key:", process.env.REACT_APP_FIREBASE_API_KEY);
 
 const db = firebase.firestore();
-
 export { db, firebase };
